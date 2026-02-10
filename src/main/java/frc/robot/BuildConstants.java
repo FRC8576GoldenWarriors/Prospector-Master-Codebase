39e3ab69e58ea40b9f547304b6dd784e8eb8c385
Teerth <readitb@gmail.com>
2026-02-10 17:55:16 -0600

    I added code for Mr.Padu's calculations for optimal velocity, but also included hood optimal angle calculations which should be moved over once the hood subsystem is created, but I dont see any rn and I dont know if I should just make one or not, or if somebody else is working on it lowkey

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Prospector-Master-Codebase";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "928ac187f03a10176020d47e398df47ee0c6fe20";
-  public static final String GIT_DATE = "2026-01-30 01:36:21 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-06 20:00:09 EST";
-  public static final long BUILD_UNIX_TIME = 1770426009595L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "87a9497ed1a2203e9e7270b11355a1c1f6530650";
+  public static final String GIT_DATE = "2026-02-06 20:03:43 EST";
+  public static final String GIT_BRANCH = "Shooter-Subsystem";
+  public static final String BUILD_DATE = "2026-02-07 14:04:38 EST";
+  public static final long BUILD_UNIX_TIME = 1770491078339L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
