ee2ac2c0a64bccd7767224772712f28ce52254a7
Teerth <readitb@gmail.com>
2026-02-14 16:30:25 -0600

    Commit from new branch and up-to-date repo

---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems.Shooter;
+
+import edu.wpi.first.units.measure.*;
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.util.Units;
+
+public class ShooterConstants {
+    // Motor IDs (Subset 30-40)
+    public static final int LEFT_SHOOTER_ID = 31;
+    public static final int RIGHT_SHOOTER_ID = 32;
+
+    // Current Limits
+    public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(40);
+    public static final Current STATOR_CURRENT_LIMIT = Amps.of(60);
+
+    // PIDFF Constants (untuned)
+    public static final double kP = 0.11;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
+    public static final double kV = 0.12;
+    public static final double kS = 0.24;
+
+    // Manual Testing Constants
+    public static final double MANUAL_STEP_RPM = 10.0;
+
+    // Hardware Constants
+    public static final double SHOOTER_HEIGHT_METERS = Units.inchesToMeters(27.25); // When the shooter pivot is in the top position
+
+    // Measurment Constants
+    public static final double RADIUS_OF_HUB_METERS = Units.inchesToMeters(41.73);
+
+}
