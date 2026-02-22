b2c9f17bf069f2b16b2b6ea8d8eee7933f6a6f9d
Teerth <readitb@gmail.com>
2026-02-22 16:13:38 -0600

    reverting SmartDashboard

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -2,27 +2,23 @@ package frc.robot.subsystems.Shooter;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.units.measure.*;
 import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.ShooterHood.ShooterHood;
 
 
 public class Shooter extends SubsystemBase {
-
-  private final LoggedNetworkNumber manualRPMTargetInput = new LoggedNetworkNumber("Shooter/ManualRPMTarget", 0);
-
-  private final LoggedNetworkNumber manualDistanceMetersInput = new LoggedNetworkNumber("Shooter/ManualDistanceMeters", 0);
-
   private final ShooterIO io;
   private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
-
-  private final Alert leftMotorAlert = new Alert("The Left Motor is disconnected", AlertType.kError);
-  private final Alert rightMotorAlert = new Alert("The Right Motor is disconnected", AlertType.kError);
+  private Alert leftMotorAlert = new Alert("The Left Motor is disconnected", AlertType.kError);
+  private Alert rightMotorAlert = new Alert("The Right Motor is disconnected", AlertType.kError);
 
   public enum ShooterStates {
     IDLE,
@@ -47,26 +43,23 @@ public class Shooter extends SubsystemBase {
     this.currentState = state;
   }
 
+
   @Override
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Shooter", inputs);
 
-    //Establishing and updating value of RPM
-    manualRPMTarget = SmartDashboard.getNumber("ManualRPMTarget: ", manualRPMTarget);
-    SmartDashboard.putNumber("Enter Value for ManualRPMTarget: ", manualRPMTarget);
+    //Read editable LoggerBoard inputs
+    manualRPMTarget = manualRPMTargetInput.get();
+    double manualDistanceMeters = manualDistanceMetersInput.get();
 
-    //For now we are using manual shooter to hub distance till we get limelight data
-    double manualDistanceMeters = SmartDashboard.getNumber("Shooter to Hub ManualDistanceMeters: ", 0.0);
-    SmartDashboard.putNumber("ManualDistanceMeters", manualDistanceMeters);
+    //Log shooter RPMs to LoggerBoard
+    Logger.recordOutput("Shooter/LeftRPM", inputs.leftMotorSpeed.in(RotationsPerSecond) * 60);
+    Logger.recordOutput("Shooter/RightRPM", inputs.rightMotorSpeed.in(RotationsPerSecond) * 60);
+    Logger.recordOutput("Shooter/ManualDistanceMeters", manualDistanceMeters);
 
-    //SmartDashboard Outputs
-    SmartDashboard.putNumber("Shooter/LeftRPM", inputs.leftMotorSpeed);
-    SmartDashboard.putNumber("Shooter/RightRPM", inputs.rightMotorSpeede);
-
-    if (!DriverStation.isDisabled()) {
+    if(!DriverStation.isDisabled()) {
       switch (currentState) {
-
         case IDLE:
           manualRPMTarget = 0;
           io.stop();
@@ -80,13 +73,15 @@ public class Shooter extends SubsystemBase {
         case VOLTAGE_CONTROL_POSITIVE:
           manualRPMTarget += ShooterConstants.MANUAL_STEP_RPM;
           manualRPMTarget = Math.min(manualRPMTarget, 6000);
-          io.setShooterVelocity(RotationsPerSecond.of(manualRPMTarget / 60.0), RotationsPerSecond.of(manualRPMTarget / 60.0));
+          io.setShooterVelocity(RotationsPerSecond.of(manualRPMTarget / 60.0),
+                                RotationsPerSecond.of(manualRPMTarget / 60.0));
           break;
 
         case VOLTAGE_CONTROL_NEGATIVE:
           manualRPMTarget -= ShooterConstants.MANUAL_STEP_RPM;
           manualRPMTarget = Math.max(manualRPMTarget, 0);
-          io.setShooterVelocity(RotationsPerSecond.of(manualRPMTarget / 60.0), RotationsPerSecond.of(manualRPMTarget / 60.0));
+          io.setShooterVelocity(RotationsPerSecond.of(manualRPMTarget / 60.0),
+                                RotationsPerSecond.of(manualRPMTarget / 60.0));
           break;
       }
     } else {
@@ -95,5 +90,7 @@ public class Shooter extends SubsystemBase {
 
     leftMotorAlert.set(!inputs.leftMotorConnected);
     rightMotorAlert.set(!inputs.rightMotorConnected);
+
+
   }
 }
