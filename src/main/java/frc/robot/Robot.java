1459395971c4d7bbcd3234e45bb4ffc98759627e
Masterpro15 <evanckurian@gmail.com>
2026-02-22 21:05:26 -0600

    tuned shooter?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Shooter.Shooter.ShooterStates;
+import frc.robot.subsystems.transport.Transport.TransportStates;
 
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.seasonspecific.reefscape2025.ReefscapeCoralOnFly;
@@ -141,6 +142,8 @@ public class Robot extends LoggedRobot {
     @Override
     public void disabledPeriodic() {
         RobotContainer.shooter.setWantedState(ShooterStates.IDLE);
+        RobotContainer.transport.setWantedState(TransportStates.Idle);
+            //ee
     }
 
     /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
