6e6d880a85e4ee35548ad5c0637cad40c19b7b03
rushsahay <rushhaank.sahay@gmail.com>
2026-02-25 00:02:47 -0600

    Added package name to Shooter Hood Util

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Prospector-Master-Codebase";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 50;
-  public static final String GIT_SHA = "f454f1b36dce8d8836d6f4c7ab2e063821cd7a15";
-  public static final String GIT_DATE = "2026-02-14 17:50:25 EST";
-  public static final String GIT_BRANCH = "Aletrs";
-  public static final String BUILD_DATE = "2026-02-14 21:06:48 EST";
-  public static final long BUILD_UNIX_TIME = 1771121208488L;
+  public static final int GIT_REVISION = 73;
+  public static final String GIT_SHA = "b33ccdd1a954b7798beb4c08a83fa46b74aa35a5";
+  public static final String GIT_DATE = "2026-02-25 00:53:50 EST";
+  public static final String GIT_BRANCH = "aletrs_teerth";
+  public static final String BUILD_DATE = "2026-02-25 01:00:32 EST";
+  public static final long BUILD_UNIX_TIME = 1771999232932L;
   public static final int DIRTY = 1;
+
   private BuildConstants(){}
 }
