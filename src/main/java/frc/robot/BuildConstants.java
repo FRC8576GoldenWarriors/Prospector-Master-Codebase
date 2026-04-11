3084223e2f948a7b69032c676acf938f691768f8
Aashrit K. <135677758+M3hri0@users.noreply.github.com>
2026-04-11 15:18:23 -0500

    Revert "Update VisionIOLimelight.java"

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Prospector-Master-Codebase";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 195;
-  public static final String GIT_SHA = "0524822ee3fde78945d1a3b6ac54956508b6545f";
-  public static final String GIT_DATE = "2026-04-11 15:44:24 EDT";
-  public static final String GIT_BRANCH = "visionRewind";
-  public static final String BUILD_DATE = "2026-04-11 16:02:22 EDT";
-  public static final long BUILD_UNIX_TIME = 1775937742468L;
+  public static final int GIT_REVISION = 193;
+  public static final String GIT_SHA = "215be463606378284abcfdbf0ff442f18befaa12";
+  public static final String GIT_DATE = "2026-04-07 21:35:38 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-04-08 22:19:20 EDT";
+  public static final long BUILD_UNIX_TIME = 1775701160815L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
